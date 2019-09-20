37d79285f09dced2369b717f95b495408520af0a
doanmade <43007082+doanmade@users.noreply.github.com>
2019-09-20 10:54:21 -0700

    Got Strech

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -9,6 +9,7 @@ const ColorList = ({ colors, updateColors }) => {
   console.log("Colors", colors);
   const [editing, setEditing] = useState(false);
   const [colorToEdit, setColorToEdit] = useState(initialColor);
+  const [colorToAdd, setColorToAdd] = useState(initialColor);
 
   const editColor = color => {
     setEditing(true);
@@ -40,10 +41,21 @@ const ColorList = ({ colors, updateColors }) => {
   const deleteColor = color => {
     // make a delete request to delete this color
     axize()
-      .delete(`http://localhost:5000/api/color/${color.id}`)
+      .delete(`http://localhost:5000/api/colors/${color.id}`)
       .then(rez => {
         console.log("Delete Color", rez);
         updateColors(color.filter(color => color.id !== rez.data));
+      })
+      .catch(errs => console.log(errs.response));
+  };
+
+  const addColor = () => {
+    axize()
+      .post(`http://localhost:5000/api/colors`, colorToAdd)
+      .then(rez => {
+        console.log("Add Color", rez);
+        updateColors(rez.data);
+        setColorToAdd(initialColor);
       })
       .catch(errs => console.log(errs.response));
   };
@@ -68,7 +80,7 @@ const ColorList = ({ colors, updateColors }) => {
         ))}
       </ul>
       {editing && (
-        <form onSubmit={saveEdit}>
+        <form>
           <legend>edit color</legend>
           <label>
             color name:
@@ -92,11 +104,50 @@ const ColorList = ({ colors, updateColors }) => {
             />
           </label>
           <div className="button-row">
-            <button type="submit">save</button>
+            <button onClick={saveEdit}>save</button>
+            {/* <button onClick={addColor}>Add</button> */}
             <button onClick={() => setEditing(false)}>cancel</button>
           </div>
         </form>
       )}
+      <form>
+        <h2>Add Color</h2>
+
+        <label>
+          color name:
+          <input
+            type="text"
+            name="color"
+            onChange={event =>
+              setColorToAdd({ ...colorToAdd, color: event.target.value })
+            }
+            placeholder="color name"
+            value={colorToAdd.color}
+          />
+        </label>
+
+        <label>
+          hex code:
+          <input
+            type="text"
+            name="hex"
+            onChange={event =>
+              setColorToAdd({
+                ...colorToAdd,
+                code: { hex: event.target.value }
+              })
+            }
+            placeholder="hex code"
+            value={colorToAdd.code.hex}
+          />
+        </label>
+
+        <div className="button-row">
+          <button type="button" onClick={() => addColor()}>
+            submit
+          </button>
+        </div>
+      </form>
       <div className="spacer" />
       {/* stretch - build another form here to add a color */}
     </div>
